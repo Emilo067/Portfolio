8b2c66a06ed4f5326f8b0e67c8dfc6555c83b920
Emilo067 <gasanovemil467@gmail.com>
2023-08-19 06:43:41 +0300

    Change Main Section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -8,43 +8,54 @@ import {Button} from "../../../components/Button";
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper justify={"space-around"}>
-                <Info>
-                    <AboutMe>
-                    <Title>About Me</Title>
+            <MainTitle>
+                <SubTitle>My name is Emil Gasanov</SubTitle>
+                <Title>Front - End Developer (React)</Title>
+            </MainTitle>
+        <FlexWrapper justify={"space-around"}>
+            <Info>
+                <AboutMe>
+                    <SubTitle>About Me</SubTitle>
                     <Description>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                         eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </Description>
-                    </AboutMe>
-                    <PersonalInfo>
-                        <TitleInfo>Personal Info</TitleInfo>
-                        <Description>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </Description>
-                    </PersonalInfo>
                     <Button>Let’s Begin</Button>
-                </Info>
-                <Image src={photo}/>
+                </AboutMe>
+                <PersonalInfo>
+                    <SubTitle>Personal Info</SubTitle>
+                    <Description>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </Description>
+                </PersonalInfo>
 
-            </FlexWrapper>
+            </Info>
+
+            <Image src={photo}/>
+        </FlexWrapper>
 
         </StyledMain>
     );
 };
 
 const StyledMain = styled.section`
- min-height: 100vh;
-  background-color: rgba(92,223,48,0.67);
+  min-height: 100vh;
+  background-color: rgba(92, 223, 48, 0.67);
 `
 
 const Info = styled.div`
   max-width: 670px;
   width: 100%;
+  display: flex;
+  align-items: center;
   ${Description} {
     width: 334px;
   }
+`
+
+const MainTitle = styled.div`
+  text-align: center;
 `
 
 const Title = styled.h1`
@@ -59,13 +70,13 @@ const Image = styled.img`
 `
 
 const AboutMe = styled.div`
- max-width: 50%;
+  max-width: 50%;
 `
 
 const PersonalInfo = styled.div`
-
+  max-width: 50%;
 `
 
-const TitleInfo = styled.h2`
+const SubTitle = styled.h2`
 
 `
