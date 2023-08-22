a79d0f03efa8b7b5a00b005e30f3817eec088bfa
Emilo067 <gasanovemil467@gmail.com>
2023-08-22 10:08:13 +0300

    Create Container.ts

---
+++ src/components/Container.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1140px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 15px;
+  outline: 1px solid red;
+  min-height: 100%;
+`
