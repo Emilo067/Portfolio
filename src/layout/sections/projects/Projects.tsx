87cd052543b0afc5a2faa91ffa6dd87a2e9dc6b7
Emilo067 <gasanovemil467@gmail.com>
2024-07-29 12:39:30 +0300

    add projects

--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -4,23 +4,33 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Project} from "./project/Project";
 import {Container} from "../../../components/Container";
 import {S} from "./Projects_Styles"
+import todolistImg from '../../../assets/img/todolist.jpg'
+import socialNetwork from '../../../assets/img/social-network.jpg'
 
 const workData = [
     {
-        title: "Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua."
+        img: `${todolistImg}`,
+        title: "Todolist",
+        description: "This ToDoList application, built with TypeScript, React, Redux Toolkit, CSS Modules, and Axios, allows users to manage tasks efficiently. Users can add, delete, update, and fetch tasks through an API. The app also supports pagination and user authentication for secure access.",
+        link: 'https://emilo067.github.io/todolist/'
     },
     {
-        title: "Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua."
+        img: `${socialNetwork}`,
+        title: "Social Network",
+        description: "This Social Network application is developed using TypeScript, React, Redux, and Styled Components. It enables users to interact through posts, comments, and likes, all managed through an API. The app features user authentication and a responsive design for an engaging user experience.",
+        link: 'https://emilo067.github.io/samurai-way-main/'
     },
     {
+        img: ``,
         title: "Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        link: ''
     },
     {
+        img: ``,
         title: "Title",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        link: ''
     }
 ]
 
@@ -32,7 +42,7 @@ export const Projects: React.FC = () => {
                 <FlexWrapper wrap={"wrap"} justify={"space-around"} gap={"42px"}>
 
                     {workData.map((w, index) => {
-                        return <Project title={w.title} key={index}
+                        return <Project link={w.link}  img={w.img} title={w.title} key={index}
                                         description={w.description}/>
                     })}
 
--- src/layout/sections/projects/project/Project.tsx
+++ src/layout/sections/projects/project/Project.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import {Description} from "../../../../components/Description";
-import {Button} from "../../../../components/Button";
 import projImg from "../../../../assets/img/proj.png"
 import {S} from "../Projects_Styles"
+import {Button} from "../../../../components/Button/Button";
 
 type ProjectPropsType = {
+    img: string,
     title: string,
-    description: string
+    description: string,
+    link: string
 }
 
 export const Project: React.FC<ProjectPropsType> = (props: ProjectPropsType) => {
     return (
         <S.Project>
             <S.ImageWrapper>
-                <S.Image src={projImg} alt={"project"}/>
-                <Button>Look It Up</Button>
+                <S.Image src={props.img ? props.img : projImg} alt={"project"}/>
+                {/*//<LinkButton href={props.link} target="_blank" rel="noopener noreferrer">Look It Up</LinkButton>*/}
+                <Button>
+                    <a href={props.link} target="_blank" rel="noopener noreferrer">Look It Up</a>
+                </Button>
             </S.ImageWrapper>
             <S.Title>{props.title}</S.Title>
             <Description>
