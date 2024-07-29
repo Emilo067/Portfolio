dc2b9b02ff8014755a6829c4d405ff1c74e70b58
Emilo067 <gasanovemil467@gmail.com>
2024-07-29 12:39:06 +0300

    changes

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {Button} from "../../../components/Button";
 import {Container} from "../../../components/Container";
 import draw from "../../../assets/img/drawing.png";
 import {S} from "./Main_Styles";
 import Typewriter from "typewriter-effect";
 import Tilt from 'react-parallax-tilt';
+import {Button} from "../../../components/Button/Button";
+import {Link} from "react-scroll";
+
 
 export const Main: React.FC = () => {
     return (
@@ -29,9 +31,12 @@ export const Main: React.FC = () => {
                             SPA, using
                             React(JS/TS), Redux. I would like to find project work and full-time employment in a company
                             with a modern approach to development and a collaborative team.</S.HeaderDescription>
-                        <Button>Contact</Button>
+                        <Button>
+                            <Link to="contact" smooth={true} activeClass={"active"}> Contact</Link>
+                        </Button>
                     </S.MainDescription>
-                    <Tilt className="parallax-effect" perspective={500} glareEnable={true} glareMaxOpacity={0.8} glareColor="#ffffff" glarePosition="bottom" glareBorderRadius="20px">
+                    <Tilt className="parallax-effect" perspective={500} glareEnable={true} glareMaxOpacity={0.8}
+                          glareColor="#ffffff" glarePosition="bottom" glareBorderRadius="20px">
                         <S.Image>
                             <img src={draw} alt={"draw"}/>
                         </S.Image>
