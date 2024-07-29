68e3f1ae8102d873dd25c0bb1695a672f342a48d
Emilo067 <gasanovemil467@gmail.com>
2024-07-29 12:40:24 +0300

    changes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,8 @@ export const theme = {
         primaryBg: '#0F1624',
         secondaryBg: '#161D2A',
 
+        accent: '#00BCD4',
+
         font: '#ffffff',
 
         gradient: 'linear-gradient(270deg, #13ADC7 0%, #6978D1 66.67%, #945DD6 100%)',
